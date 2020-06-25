40200c23c2a668c26922a64225b702f394ccc865
Alex Chesters <10558792+AlexChesters@users.noreply.github.com>
2020-06-25 13:40:23 +0100

    remove unused files

--- scripts/pipeline.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-AWS_PROFILE=personal \
-  aws cloudformation deploy \
-  --template-file ci/codepipeline.yml \
-  --stack-name codepipeline-codebuild-images \
-  --capabilities CAPABILITY_IAM \
-  --region eu-west-1
